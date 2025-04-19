5af80b84f1f650ac23ce62b89f4f80998d89c199
dragonheart8787 <134299027+dragonheart8787@users.noreply.github.com>
2025-04-19 21:17:31 +0800

    Create Footer.tsx

---
+++ components/components/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-8 bg-gray-800 text-gray-400 text-center">
+      © 2025 YourName. All rights reserved.
+    </footer>
+  )
+}
